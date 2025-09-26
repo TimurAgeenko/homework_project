74129eb9fab57bf9bce12f8d70745d6f9f953fd0
TimurAgeenko <ageenko.timur.10@gmail.com>
2025-09-26 21:40:56 +0300

    added sort function

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,9 @@
-def filter_by_state(info: list[dict], state: str = 'EXECUTED') -> list[dict]:
+import datetime as dt
+
+from src.widjet import get_date
+
+
+def filter_by_state(info: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Принимает список с информацией и возвращает отредактированный список,
     в котором содержатся только словари, у которых ключ state принимает указанное значение"""
     result = []
@@ -6,5 +11,12 @@ def filter_by_state(info: list[dict], state: str = 'EXECUTED') -> list[dict]:
     for item in info:
         if item["state"] == state:
             result.append(item)
+
+    return result
+
+
+def sort_by_date(info: list[dict], reverse: bool = True) -> list[dict]:
+    """Принимает список и возвращает его отсортированный по дате вариант, по умолчанию - по убыванию"""
+    result = sorted(info, key=lambda x: dt.datetime.strptime(get_date(x["date"]), "%d.%m.%Y"), reverse=reverse)
 
     return result
