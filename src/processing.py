cc1f66b1b9b85211e4bf413ccefb4a0eb23ed1f9
TimurAgeenko <ageenko.timur.10@gmail.com>
2025-09-26 18:53:57 +0300

    added filter function

---
+++ src/processing.py
@@ -0,0 +1,10 @@
+def filter_by_state(info: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """Принимает список с информацией и возвращает отредактированный список,
+    в котором содержатся только словари, у которых ключ state принимает указанное значение"""
+    result = []
+
+    for item in info:
+        if item[state] == state:
+            result.append(item)
+
+    return result
