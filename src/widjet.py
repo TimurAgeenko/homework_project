d776dbf001a696f3bde84f3d9de560145e4993b3
TimurAgeenko <ageenko.timur.10@gmail.com>
2025-09-28 14:46:34 +0300

    added test_mask_account_card and fixed the current function

--- src/widjet.py
+++ src/widjet.py
@@ -3,18 +3,38 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(account_card_num: str) -> str:
     """Принимает тип и номер карты или счета и возвращает замаскированный вариант"""
+    if not isinstance(account_card_num, str):
+        raise TypeError("Входные данные должны иметь тип str")
+
     account_card_num_list = account_card_num.split()
+    digits_counter = 0
+
+    for item in account_card_num_list:
+        if item.isdigit():
+            digits_counter += 1
+
+    if digits_counter == 0:
+        return "В номере не должно быть букв"
+
+    if digits_counter > 1:
+        return "В номере не должно быть пробелов"
 
     if "Счет" in account_card_num_list:
         for item in account_card_num_list:
             item_index = account_card_num_list.index(item)
             if item.isdigit():
-                account_card_num_list[item_index] = get_mask_account(item)
+                if len(item) == 20:
+                    account_card_num_list[item_index] = get_mask_account(item)
+                else:
+                    return "Номер счета должен состоять из 20 цифр"
     else:
         for item in account_card_num_list:
             item_index = account_card_num_list.index(item)
             if item.isdigit():
-                account_card_num_list[item_index] = get_mask_card_number(item)
+                if len(item) == 16:
+                    account_card_num_list[item_index] = get_mask_card_number(item)
+                else:
+                    return "Номер карты должен состоять из 16 цифр"
 
     result = " ".join(account_card_num_list)
 
@@ -25,10 +45,13 @@ def get_date(date: str) -> str:
     """Принимает строку с датой в одном формате и возвращает в другом"""
     if not isinstance(date, str):
         raise TypeError("Дата должна иметь тип str")
+
     elif len(date) != 26:
         return "Указан неверный формат даты"
+
     elif date[8:10].isdigit() and date[5:7].isdigit() and date[:4].isdigit():
         result = date[8:10] + "." + date[5:7] + "." + date[:4]
         return result
+
     else:
         return "Дата не должна содержать букв"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.widjet import mask_account_card, get_date
 
+
 def test_get_date_type():
     with pytest.raises(TypeError) as exc_info:
         get_date(int)
@@ -17,3 +18,28 @@ def test_get_date_type():
 ])
 def test_get_date(value, expected):
     assert get_date(value) == expected
+
+
+def test_mask_account_card_type():
+    with pytest.raises(TypeError) as exc_info:
+        mask_account_card(int)
+
+    assert str(exc_info.value) == "Входные данные должны иметь тип str"
+
+
+@pytest.mark.parametrize("value, expected", [
+    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("Счет 73654108430135874305", "Счет **4305"),
+    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Счет 7365410843013587grrg", "В номере не должно быть букв"),
+    ("Visa Platinum 8990922dfs665229", "В номере не должно быть букв"),
+    ("Счет 73654108430135874305328348", "Номер счета должен состоять из 20 цифр"),
+    ("Счет 3538303347444", "Номер счета должен состоять из 20 цифр"),
+    ("Visa Gold 5999414228426353342341", "Номер карты должен состоять из 16 цифр"),
+    ("Visa Gold 5999414228", "Номер карты должен состоять из 16 цифр"),
+    ("Счет 7365410843 0135874305", "В номере не должно быть пробелов"),
+    ("Visa Platinum 7000792289 606361", "В номере не должно быть пробелов")
+])
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
